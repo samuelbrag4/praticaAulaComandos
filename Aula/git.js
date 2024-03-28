313fefeccfaf27a2f6e4e502e032a4be3f6f7922
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-03-28 08:36:43 -0300

    Meu primeiro commit de teste

---
+++ Aula/git.js
@@ -0,0 +1,5 @@
+let fahrenheit = 98;
+let celsius = (fahrenheit - 32) * 5 / 9;
+let celsiusarredondado = celsius.toFixed(1);
+
+console.log("bem vindo! A temperatura em celsius é de " + celsiusarredondado + "c°" + ", e em fahrenheit " +  fahrenheit + " f°.");
